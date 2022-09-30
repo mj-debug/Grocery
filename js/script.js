ebf79c2dc7a3ec1d691dc4ff7297b5fca85004cc
mj <majestyyolo@gmail.com>
2022-09-30 02:26:43 -0700

    second commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Mobile Nav bar
+
+function toggleSideNav(){
+    if(sideNav.style.right == '-250px'){
+        sideNav.style.right = '0';
+    }else{
+        sideNav.style.right = '-250px';
+    }
+}
